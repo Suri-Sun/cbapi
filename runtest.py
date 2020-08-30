a9f6830d3724eb77b52d1ed908abfd8cdcdcc322
Suri-Sun <68470988+Suri-Sun@users.noreply.github.com>
2020-08-31 00:20:49 +0800

    Update runtest.py

--- runtest.py
+++ runtest.py
@@ -4,7 +4,7 @@ import cbapi
 
 def test_cbapi():
 
-    cbapi.set_key("fcc6d1909dmsha9e61b6e0048f1cp1b804ejsn16302603dc9a")  # hide it later
+    cbapi.set_key(<YOUR_RAPIDAPI_KEY>)  # hide it later
 
     print(">>", "MadlyRad Labs, Inc.", end=' ... \n')
     org = cbapi.get_org(name="MadlyRad Labs, Inc.")
